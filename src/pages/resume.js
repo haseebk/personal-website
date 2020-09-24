1724018f9ab46fd88085763dd34aabdd7865693f
Haseeb <haseebadilkhan@gmail.com>
2020-09-24 13:53:34 -0600

    revert

---
+++ src/pages/resume.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import Section from '../components/Section';
+import Triangle from '../components/Triangle';
+import ResumePDF from './resume/resume.pdf';
+
+const Background = () => (
+  <div>
+    <Triangle
+      color="backgroundDark"
+      height={['35vh', '80vh']}
+      width={['95vw', '60vw']}
+    />
+
+    <Triangle
+      color="secondary"
+      height={['25vh', '35vh']}
+      width={['75vw', '60vw']}
+      invertX
+    />
+
+    <Triangle
+      color="primaryDark"
+      height={['10vh', '20vh']}
+      width={['50vw', '50vw']}
+      invertX
+      invertY
+    />
+  </div>
+);
+
+const Resume = ({ resume }) => (
+  <Layout>
+    <embed
+        src={ResumePDF}
+        type="application/pdf"
+        width="100%"
+        height="600px"
+        title="resume"
+        data="resume.pdf?#zoom=100&scrollbar=1&toolbar=1&navpanes=0"
+      ></embed>
+
+    {/* <Section.Container id="Resume" Background={Background}>
+      <embed
+        src={ResumePDF}
+        type="application/pdf"
+        width="100%"
+        height="100%"
+      />
+    </Section.Container> */}
+  </Layout>
+);
+
+export default Resume;
