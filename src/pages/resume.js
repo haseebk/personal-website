5088e322d99379cc93b5ae050a6ab4849cb29eaa
Haseeb <haseebadilkhan@gmail.com>
2020-09-24 01:06:20 -0600

    t

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -39,7 +39,7 @@ const Resume = ({ resume }) => (
           src={ResumePDF}
           type="application/pdf"
           width="1200px"
-          height="900px"
+          height="200%"
           title="resume"
           data="resume.pdf?#zoom=100&scrollbar=1&toolbar=1&navpanes=1"
         />
