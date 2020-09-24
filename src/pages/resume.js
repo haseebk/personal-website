ec5c876423e3a901d74b6587d8c3f0c843162652
Haseeb <haseebadilkhan@gmail.com>
2020-09-24 12:43:00 -0600

    test

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -33,17 +33,6 @@ const Background = () => (
 
 const Resume = ({ resume }) => (
   <Layout>
-    <div id="resumePDF">
-      {/* <div id="resumePDF" style="height: 100%"> */}
-      <embed
-        src={ResumePDF}
-        width="100%"
-        height="100vh"
-        type="application/pdf"
-        title="resume"
-        data="resume.pdf?#zoom=100&scrollbar=1&toolbar=1&navpanes=1"
-      ></embed>
-    </div>
 
     {/* <embed
         src={ResumePDF}
@@ -64,14 +53,14 @@ const Resume = ({ resume }) => (
         </Box>
     </Section.Container> */}
 
-    {/* <Section.Container id="Resume" Background={Background}>
+    <Section.Container id="Resume" Background={Background}>
       <embed
         src={ResumePDF}
         type="application/pdf"
         width="100%"
         height="100vh"
       />
-    </Section.Container> */}
+    </Section.Container>
   </Layout>
 );
 
