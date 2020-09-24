da7bdcfa3f70d14a1a8b17ca2c8f203dd2febe53
Haseeb <haseebadilkhan@gmail.com>
2020-09-24 02:23:46 -0600

    trying 150vh

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -36,7 +36,7 @@ const Resume = ({ resume }) => (
       <embed
         src={ResumePDF}
         width="100%"
-        height="50vw"
+        height="150vw"
         type="application/pdf"
         title="resume"
         data="resume.pdf?#zoom=100&scrollbar=1&toolbar=1&navpanes=1"
