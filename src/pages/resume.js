1615d4c3e65d3f6722ad54a58946926f564b5a74
Haseeb <haseebadilkhan@gmail.com>
2020-10-16 02:23:15 -0600

    testing object

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -52,16 +52,13 @@ const Resume = ({ resume }) => (
 
 {/* <iframe src={ResumePDF}
 width="100%" height="300px"  /> */}
-  <div id="x"></div>
 
-<script>
-var img = document.createElement("img");
- 
-img.src = {ResumeIMG};
-var src = document.getElementById("x");
- 
-src.appendChild(img);
-</script>
+
+<object data={ResumePDF} type="application/pdf" width="700px" height="700px">
+    <embed src={ResumePDF}>
+        <p>This browser does not support PDFs. Please download the PDF to view it: <a href="http://yoursite.com/the.pdf">Download PDF</a>.</p>
+    </embed>
+</object>
   
   </Layout>
 );
