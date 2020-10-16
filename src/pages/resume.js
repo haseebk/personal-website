390a3988a4678eb1e99e1ee69bb311759068ae52
Haseeb <haseebadilkhan@gmail.com>
2020-10-16 02:01:52 -0600

    testing percentage sizing for resume

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -51,7 +51,7 @@ const Resume = ({ resume }) => (
 
 <iframe src={ResumePDF}
 // pid=explorer&efh=false&a=v&chrome=false&embedded=true"
- width="400px" height="300px"  />
+ width="100%" height="100%"  />
   </Layout>
 );
 
