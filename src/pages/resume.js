43a49257d18f1a003c03031d2b493203fd7ba4e7
Haseeb <haseebadilkhan@gmail.com>
2020-11-01 01:13:47 -0700

    changing resume view to fit

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -34,7 +34,8 @@ const Resume = ({ resume }) => (
     <RSection.RContainer id="Resume" Background={Background}>
       <iframe
       
-        src={ResumePDF}
+        // src={ResumePDF}
+        src="/static/resume-8cb3b42d6cfd821c5fd499843f6ff050.pdf#view=Fit"
         // src="/static/resume-eafacdbe81755ae6651a07e0c577c6de.pdf#view=Fit"
         type="application/pdf"
         alt="Resume"
