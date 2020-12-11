f7a3d9db50972baba8ba404f7ae7df6ee11223c6
Haseeb <haseebadilkhan@gmail.com>
2020-12-10 19:54:08 -0700

    changing resume src to viewFIT

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -34,8 +34,8 @@ const Resume = ({ resume }) => (
     <RSection.RContainer id="Resume" Background={Background}>
       <iframe
       
-        src={ResumePDF}
-        // src="/static/resume-8cb3b42d6cfd821c5fd499843f6ff050.pdf#view=Fit"
+        // src={ResumePDF}
+        src="/static/resume-24af322a5cd7011f9db72f91768dd0b1.pdf#view=Fit"
         // src="/static/resume-eafacdbe81755ae6651a07e0c577c6de.pdf#view=Fit"
         type="application/pdf"
         alt="Resume"
